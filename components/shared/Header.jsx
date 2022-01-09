4980fb83f0da62b35d5f82d41153153ff0e57323
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 02:30:50 -0300

    improve Header readability

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -4,12 +4,20 @@ const Header = ({ data, darkTheme }) => {
   return (
     <header className="flex items-center w-full h-16">
       <Link href="/">
-        <a className={`ml-8 mr-4 font-bold ${darkTheme && "text-gray-50"}`}>
+        <a
+          className={`ml-8 mr-4 text-lg font-bold ${
+            darkTheme && "text-gray-50"
+          }`}
+        >
           Home
         </a>
       </Link>
       <Link href="/createForm">
-        <a className={`ml-4 mr-8 font-bold ${darkTheme && "text-gray-50"}`}>
+        <a
+          className={`ml-4 mr-8 text-lg font-bold ${
+            darkTheme && "text-gray-50"
+          }`}
+        >
           Create new form
         </a>
       </Link>
