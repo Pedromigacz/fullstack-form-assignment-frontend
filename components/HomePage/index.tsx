06ccc28aabb181820e49b14c372f180397a07955
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 01:54:50 -0300

    quickstart tailwindcss

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const HomePageLayout: NextPage = ({ data }) => {
-  return <h1>Landing page</h1>;
+  return <h1 className="text-blue-500">Landing page</h1>;
 };
 
 export default HomePageLayout;
