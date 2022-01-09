690850b1da36897316747d8426c802918ff9bad7
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 16:05:57 -0300

    add backend env variable

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  uri: process.env.BACKEND_ENDPOINT || "http://localhost:1337/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
