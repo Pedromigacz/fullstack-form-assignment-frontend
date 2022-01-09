98e2e48429c80ce5bf233f9f281b065a0a11ced8
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 10:38:10 -0300

    remove duplicated sort

--- components/EditFormPage/Aside.tsx
+++ components/EditFormPage/Aside.tsx
@@ -98,38 +98,36 @@ const Aside = ({ form, setForm }) => {
       </div>
       <div className="w-full">
         <p className="block text-2xl font-bold mb-6">Fields:</p>
-        {[...form.fields]
-          .sort((prev, curr) => prev.sort_index - curr.sort_index)
-          .map((field, key) => (
-            <div
-              key={key}
-              className="w-full bg-zinc-200 rounded-md flex overflow-hidden mb-4"
+        {[...form.fields].map((field, key) => (
+          <div
+            key={key}
+            className="w-full bg-zinc-200 rounded-md flex overflow-hidden mb-4"
+          >
+            <input
+              type="text"
+              placeHolder="Label"
+              className="bg-zinc-200 border-none outline-0 px-4 py-3 inline-block"
+              value={field.label}
+              onChange={(e) => {
+                updateName(e, field);
+              }}
+            />
+            <button className="ml-auto p-2">
+              <ArrowNarrowUpIcon className="h-5 w-5 mr-2" />
+            </button>
+            <button className="p-2">
+              <ArrowNarrowDownIcon className="h-5 w-5 mr-2" />
+            </button>
+            <button
+              className="p-2"
+              onClick={(e) => {
+                removeField(key);
+              }}
             >
-              <input
-                type="text"
-                placeHolder="Label"
-                className="bg-zinc-200 border-none outline-0 px-4 py-3 inline-block"
-                value={field.label}
-                onChange={(e) => {
-                  updateName(e, field);
-                }}
-              />
-              <button className="ml-auto p-2">
-                <ArrowNarrowUpIcon className="h-5 w-5 mr-2" />
-              </button>
-              <button className="p-2">
-                <ArrowNarrowDownIcon className="h-5 w-5 mr-2" />
-              </button>
-              <button
-                className="p-2"
-                onClick={(e) => {
-                  removeField(key);
-                }}
-              >
-                <XIcon className="h-5 w-5 mr-2 text-orange-700" />
-              </button>
-            </div>
-          ))}
+              <XIcon className="h-5 w-5 mr-2 text-orange-700" />
+            </button>
+          </div>
+        ))}
         <button
           className="bg-cyan-600 text-gray-50 px-4 py-2 rounded-md text-lg"
           onClick={addField}
