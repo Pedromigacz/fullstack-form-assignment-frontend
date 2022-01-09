fbeb1de519de21470861657029c2037ba333df3f
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 18:52:06 -0300

    rename BACKEND_ENDPOINT env var to NEXT_BACKEND_ENDPOINT

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 export const client = new ApolloClient({
-  uri: process.env.BACKEND_ENDPOINT || "http://localhost:1337/graphql",
+  uri: process.env.NEXT_BACKEND_ENDPOINT || "http://localhost:1337/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
