af1506214d3cb5138726c79295ccbba638fc9846
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 14:29:34 -0300

    add react-form-hook to FormPreview component

--- components/shared/FormPreview/index.tsx
+++ components/shared/FormPreview/index.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const FormPreview = ({ form }) => {
+  const { handleSubmit, register } = useForm();
+
+  const onSubmit = (e) => console.log(e);
+
   return (
-    <form className="mx-8 mt-2 text-zinc-50">
+    <form onSubmit={handleSubmit(onSubmit)} className="mx-8 mt-2 text-zinc-50">
       <h2 className="text-2xl font-bold mb-8">
         Form preview: &quot;{form.name}&rdquo;
       </h2>
@@ -10,7 +15,11 @@ const FormPreview = ({ form }) => {
       {form.fields.map((field, key) => (
         <div className="mt-1" key={key}>
           <label className="block font-semibold ml-6">{field.label}:</label>
-          <input className="border-none bg-zinc-800 rounded-md px-6 py-4 mt-2 mb-4 w-full text-xl font-semibold" />
+          <input
+            className="border-none bg-zinc-800 rounded-md px-6 py-4 mt-2 mb-4 w-full text-xl font-semibold"
+            name={field.label}
+            {...register(field.label)}
+          />
         </div>
       ))}
       <button
