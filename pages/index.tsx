fe8639006bf20368b7a8b2fd50c1d3b27429d94e
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-09 01:27:46 -0300

    add first query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
 import type { NextPage } from "next";
+import { gql, useQuery, useMutation } from "@apollo/client";
+
+const AllLinksQuery = gql`
+  query {
+    forms {
+      id
+      name
+      fields {
+        id
+      }
+    }
+  }
+`;
 
 const Home: NextPage = () => {
+  const { data, loading, error } = useQuery(AllLinksQuery);
+
+  if (loading) return <h1>Loading.....</h1>;
+  if (error) return <h1>Ops! Something went wrong</h1>;
+
+  console.log(data);
+
   return <h1>Landing page</h1>;
 };
 
